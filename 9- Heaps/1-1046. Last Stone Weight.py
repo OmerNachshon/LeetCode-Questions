12054168066ef022adf765ab7f7fc637741af81f
Omer Nachshon <Omer.Nachshon@s.afeka.ac.il>
2024-11-17 18:20:15 +0200

    add last stone weight optimal solution

--- 9- Heaps/1-1046. Last Stone Weight.py
+++ 9- Heaps/1-1046. Last Stone Weight.py
@@ -13,13 +13,16 @@ Return the weight of the last remaining stone. If there are no stones left, retu
 import heapq
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        for i in range(len(stones)):
+            stones[i] *= -1 # Negate to force Max Heap
+
         heapq.heapify(stones)
+
         while len(stones) > 1:
-            stones.sort()
-            y , x = stones[-1] , stones[-2]
-            stones = stones[:-2]
-            if x!=y:
-                heapq.heappush(stones,y-x)
-        if len(stones) == 0:
-            return 0
-        return heapq.heappop(stones)
+            largest = heapq.heappop(stones)
+            next_largest = heapq.heappop(stones)
+
+            if largest != next_largest:
+                heapq.heappush(stones, largest - next_largest)
+
+        return -heapq.heappop(stones) if stones else 0
